efc67a21ef7a307c3ed7c245491bc038aed63fd2
Steven Deobald <steven@c42.in>
2013-06-24 18:00:31 +0530

    removing the old partition impl. because we're going to change it

--- lib/frappuccino/stream/partition.rb
+++ lib/frappuccino/stream/partition.rb
@@ -1,17 +1,11 @@
 module Frappuccino
   class Partition < Stream
-    def initialize(source, n)
-      @n = n
-      @so_far = []
-      source.add_observer(self)
+    def initialize(source, &block)
+      # TODO impl.
     end
 
     def update(value)
-      @so_far << value
-      if @so_far.length == @n
-        occur(@so_far.dup)
-        @so_far.clear
-      end
+      # TODO: impl
     end
   end
 end
--- test/partition_test.rb
+++ test/partition_test.rb
@@ -1,42 +1,12 @@
 require 'test_helper'
 
 describe "partition" do
-  it "returns every 2 values as one value" do
-    button = Button.new
+  it "will partition the ruby way" do
 
-    stream = Frappuccino::Stream.new(button)
-
-    parted_stream = stream.partition(2)
-
-    parts = []
-
-    parted_stream.on_value do |event|
-      parts << event
-    end
-
-    5.times { button.push }
-
-    assert_equal [ [:pushed, :pushed],
-                   [:pushed, :pushed] ], parts
   end
 
-  it "returns every 3 values as one value" do
-    button = Button.new
+  it "will do more" do
 
-    stream = Frappuccino::Stream.new(button)
-
-    parted_stream = stream.partition(3)
-
-    parts = []
-
-    parted_stream.on_value do |event|
-      parts << event
-    end
-
-    7.times { button.push }
- 
-    assert_equal [ [:pushed, :pushed, :pushed],
-                   [:pushed, :pushed, :pushed] ], parts
   end
 
 end
